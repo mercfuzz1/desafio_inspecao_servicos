d8e5d3c00e765edb2abaeb4fad9df276e04ae9e2
mercfuzz1 <166085769+mercfuzz1@users.noreply.github.com>
2026-08-13 23:19:24 -0300

    feat: adicionando arquivo do authInterceptor no fluxo de autenticação.

---
+++ lib/core/network/auth_interceptor.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+import '../storage/secure_storage.dart';
+
+class AuthInterceptor extends Interceptor {
+  final SecureStorage secureStorage;
+
+  AuthInterceptor(this.secureStorage);
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final token = await secureStorage.getAccessToken();
+
+    if (token != null && token.isNotEmpty) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+
+    handler.next(options);
+  }
+}
